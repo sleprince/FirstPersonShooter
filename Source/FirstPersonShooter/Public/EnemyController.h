d61e81afb4830ed12e1bc2dde3c1bcf24e22db02
Stavrin <steffanleprince@gmail.com>
2023-04-29 13:26:38 +0100

    Adding new class., EnemyController.

---
+++ Source/FirstPersonShooter/Public/EnemyController.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemyController.generated.h"
+
+UCLASS() //all created classes have A at the start
+class FIRSTPERSONSHOOTER_API AEnemyController : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemyController();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
