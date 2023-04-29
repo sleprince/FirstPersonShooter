31c2eb75bbdc7adce57e33149581e59ce21bd485
Stavrin <steffanleprince@gmail.com>
2023-04-29 11:40:30 +0100

    Initial commit

---
+++ Source/FirstPersonShooter/FirstPersonShooter.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstPersonShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstPersonShooter, "FirstPersonShooter" );
+ 
